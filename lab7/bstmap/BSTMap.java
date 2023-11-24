d56e03f0dceed85156441b3a61a3de66d344c7d8
jimshaw0001 <jimshaw0001@gmail.com>
2023-11-24 15:12:55 +0000

    lab7 complete

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -96,7 +96,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V>{
 
 
     @Override
-    public Set keySet() {
+    public Set<K> keySet() {
         throw new UnsupportedOperationException();
     }
 
@@ -111,7 +111,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V>{
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
     }
 
